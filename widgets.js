9604fd772125f9601c45ff6125320468bb095410
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2022-12-23 21:29:33 -0500

    onoff works with light mode too now

--- widgets.js
+++ widgets.js
@@ -5,7 +5,7 @@ const OnOffLabel = document.getElementById('OnOffLabel');
 OnOffSwitch.addEventListener('change', (event) => {
     if (event.currentTarget.checked) {
         OnOffLabel.textContent = "On"
-        OnOffLabel.parentElement.style.backgroundColor = "#272727";
+        OnOffLabel.parentElement.style.backgroundColor = "var(--switchactive)";
         OnOffLabel.style.color = "#3E91FF";
     } else {
         OnOffLabel.textContent = "Off"
